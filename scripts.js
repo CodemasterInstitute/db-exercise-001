411b8e22df228dd9f501955e1568f4df0e3c939f
dboterho <hello@danielboterhoven.tech>
2016-03-08 06:45:18 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+function render() {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    
+    for (var i = 0; i < alphabet.length; i++) {
+        
+        var line = alphabet[i];
+        
+        outputLine(line);
+    }
+    
+}
+
+function outputLine(line) {
+    
+    var output = document.getElementById('output');
+        
+    var currentHtml = output.innerHTML;
+    
+    var newHtml = currentHtml + line + '<br />';
+    
+    output.innerHTML = newHtml;
+    
+}
