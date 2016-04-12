b5a8300b84a8d4cf0aae95ad95532a585792e1fc
Helen Burgess <me@graphitedge.com.au>
2016-04-12 10:49:31 +0800

    Updated scripts.js to show alternate solution

--- scripts.js
+++ scripts.js
@@ -1,26 +1,38 @@
-
+//declaring variable object render with a function as the content of the variable
 var render = function() {
 
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     
     var vowels = ['a', 'e', 'i', 'o', 'u'];
-    
-    for (var i = 0; i < alphabet.length; i++) {        
-        var line = alphabet[i];
+
+
+    //outer loop to go through alphabet
+    for (var i = 0; i < alphabet.length; i++) {  
+           
+        // inner loop for vowels
+        for(var j=0; j<vowels.length; j+=1){
+            //checking to see if alphabet[index] equivalent to vowels[index]            
+            if( alphabet[i] === vowels[j]){
+                alphabet[i] = alphabet[i]+ '(VOWEL)'; //adding the word Vowel in brackets to a vowel
+            }
+        }    
+        //value sent to outputLine function-variable 
+       outputLine(alphabet[i]); 
         
-        outputLine(line);
     }
     
 }
 
-var outputLine = function(line) {
+//declaring functions for html output
+function outputLine(line) {
+    //manipulating the DOM of the browser
     
-    var output = document.getElementById('output');
+    var output = document.getElementById('output'); //finding the element to be manipulated
         
-    var currentHtml = output.innerHTML;
+    var currentHtml = output.innerHTML; //assigning current text value of element 
     
-    var newHtml = currentHtml + line + '<br />';
+    var newHtml = currentHtml + line + '<br />'; //creating new text value for element
     
-    output.innerHTML = newHtml;
+    output.innerHTML = newHtml; //writing the new text.
     
 }
